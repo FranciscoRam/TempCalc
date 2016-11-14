7cd50e5e373c6221e3abc2df71cdf95308c377bf
FranciscoRam <r.g.francisco40@gmail.com>
2016-11-14 02:57:57 -0600

    Agregar Butterknife Binding

--- app/src/main/java/com/example/franks/tempcalc/MainActivity.java
+++ app/src/main/java/com/example/franks/tempcalc/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.franks.tempcalc;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.input.InputManager;
 import android.net.Uri;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import android.util.Log;
 import android.view.Menu;
@@ -12,17 +15,47 @@ import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import android.view.inputmethod.InputMethod;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     Spinner spinIn;
     Spinner spinOut;
     String[] gradoIn= {"Celcius", "Fahrenheit", "Kelvin"};
 
+    @Bind(R.id.inputBill)
+    EditText inputBill;
+    @Bind(R.id.btnSubmit)
+    Button btnSubmit;
+    /*
+    @Bind(R.id.inputPercentage)
+    EditText inputPercentage;
+    */
+    @Bind(R.id.btnIncrease)
+    Button btnIncrease;
+    @Bind(R.id.btnDecrease)
+    Button btnDecrease;
+    @Bind(R.id.btnClear)
+    Button btnClear;
+    @Bind(R.id.txtTip)
+    TextView txtTip;
+
+    private final static int TIP_STEP_CHANGE = 1;
+    private final static int DEFAULT_TIP_CHANGE = 10;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
 
         spinIn = (Spinner)findViewById(R.id.spinnerIn);
 
@@ -50,6 +83,22 @@ public class MainActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    @OnClick(R.id.btnSubmit)
+    public void handleSubmit() {
+        hideKeyboard();
+    }
+
+    private void hideKeyboard() {
+        InputMethodManager inputManager= (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        try {
+            inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        } catch (NullPointerException npe) {
+            Log.e(getLocalClassName(), Log.getStackTraceString(npe));
+        }
+    }
+
+
 /*
     private void about() {
         TempCalcApp app = (TempCalcApp)getApplication();
